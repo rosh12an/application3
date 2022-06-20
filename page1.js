546e6475d31b838756df3c5e8c93d8960a46b27d
D8_62761_RoshanPal <roshan2.pal@gmail.com>
2022-06-20 11:22:30 +0530

    page 1 added

---
+++ page1.js
@@ -0,0 +1,10 @@
+function isEven(number) {
+    if (number % 2 == 0) {
+      console.log(`${number} is EVEN`)
+    } else {
+      console.log(`${number} is NOT EVEN`)
+    }
+  }
+  
+  isEven(10)
+  isEven(11)
